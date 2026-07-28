01e68cd994b7f7ec00f38ac4d35bfd5cb3871184
Scp105 <2163411547@qq.com>
2026-07-28 18:56:58 +0800

    张清耀

---
+++ 张清耀/Scripts/GameDataManger.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class GameDataManger
+    {
+        // 是否正在眨眼
+        public static bool isBlinking;
+        
+        // 玩家是否看着SCP-173
+        public static bool isWatchingSCP;
+        
+        // 游戏是否结束（玩家死亡）
+        public static bool isGameOver;
+    }
+}
